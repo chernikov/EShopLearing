5c4c7f437a404810ca3488b9fa203906a62ab926
Andriy Chernikov <andriy.chernikov@emburse.com>
2022-11-22 12:26:52 +0200

    Update backend

---
+++ EShopLearning.Api/Controllers/SecureController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShopLearning.Api.Controllers
+{
+    [Authorize]
+    [Route("api/secure")]
+    public class SecureController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Secure Information");
+        }
+    }
+}
